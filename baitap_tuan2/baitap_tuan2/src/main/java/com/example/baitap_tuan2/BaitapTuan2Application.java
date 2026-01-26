942b6386d36c20f9cb34844aa1a240b5e508ce43
thonocodesiu <thongtg7@gmail.com>
2026-01-26 09:46:45 +0700

    update bai tap

---
+++ baitap_tuan2/baitap_tuan2/src/main/java/com/example/baitap_tuan2/BaitapTuan2Application.java
@@ -0,0 +1,14 @@
+package com.example.baitap_tuan2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaitapTuan2Application {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(BaitapTuan2Application.class, args);
+	}
+
+}
---
+++ baitap_tuan2/baitap_tuan2/src/test/java/com/example/baitap_tuan2/BaitapTuan2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baitap_tuan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaitapTuan2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
